9021c738758ab5efc29f50cd8760a84bd1e68fea
bekidink <bereketdinku876@gmail.com>
2025-06-02 14:39:44 +0300

    dashboard

---
+++ src/app/(back)/dashboard/admin/products/page.jsx
@@ -0,0 +1,31 @@
+import PageHeader from '@/components/backoffice/PageHeader'
+import DataTable from '@/components/data-table-components/DataTable'
+import { getData } from '@/lib/getData'
+
+import { columns } from './columns'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/authOptions'
+
+const page = async() => {
+  const session=await getServerSession(authOptions)
+  if(!session){
+    return null
+  }
+  const products=await getData('products')
+  const id=session?.user?.id
+  const role=session?.user?.role
+  const farmerProudcts=products.filter((product)=>product.userId===id)
+  return (
+    <div>
+     <PageHeader heading={"Products"} href={"/dashboard/products/new"} LinkTitle={"Add Product"}/>
+     <div className="py-8">
+      {role==="ADMIN"?(<DataTable data={products} columns={columns} filterKeys={['title']} />):(
+        <DataTable data={farmerProudcts} columns={columns} filterKeys={['title']} />
+      )}
+      
+     </div>
+    </div>
+  )
+}
+
+export default page
